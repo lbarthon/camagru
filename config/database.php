1f4240332d8c27e76a9da6c6fb53a981d0183c70
lbarthon <lsbarthonet2@gmail.com>
2019-01-24 08:59:37 +0100

    Config bug -- remove

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Database informations.
- */
-
-$db_dsn = "mysql:dbname=Camagru;host=localhost";
-$db_user = "lbarthon";
-$db_password = "12345678";
--- config/setup.php
+++
@@ -1,5 +0,0 @@
-<form action="/setup" method="post">
-    <input type="hidden" name="token" value="<?= $token ?>">
-    <input type="submit" name="submit" value="Setup camagru ?">
-</form>
-<p style='color: red;'><?= $error ?></p>
